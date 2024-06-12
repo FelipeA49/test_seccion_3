8cd6cd1d9a998878c742bc5af6e3666814fa0076
FelipeA49 <fe.arriagadao@duocuc.cl>
2024-06-12 15:02:08 -0400

    Add files via upload

---
+++ calculadora.py
@@ -0,0 +1,10 @@
+while True :
+    print(".-.-. menu .-.-.")
+    print("")
+    print("1.- sumar ")
+    print("2.- restar ")
+    print("3.- multiplicar ")
+    print("4.- dividir ")
+    print("0.- salir")
+    print("")
+    
